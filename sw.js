4364d9beb9a6313c9d19c3c8e72a1e524e412a5f
shinjaepal-EMT <shinjaepal@hanmail.net>
2026-09-18 10:02:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
-const CACHE='ems-clinical-pocket-v26';
+const CACHE='ems-clinical-pocket-v36';
 const ASSETS=['./','./index.html','./data.js','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener('install',e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+));
+
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(e.request.method==='GET' && (url.pathname.endsWith('/data.js') || url.pathname.endsWith('/index.html'))){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
+      const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+    }).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
